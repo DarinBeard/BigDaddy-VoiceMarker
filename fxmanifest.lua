7d55113d742db14f274fe53f115527830199cc35
Darin Beard <DarinBeard@users.noreply.github.com>
2022-06-27 11:19:20 -0400

    update manifest file

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'FiveM tool to show who is talking visually'
+author 'Big Daddy'
+version '1.0.3'
+
+client_scripts {
+	'config.lua',
+	'client.lua',
+}
+
+server_scripts {
+}
+
